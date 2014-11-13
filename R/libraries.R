9438ca80ae8d0ef4dcdea321b7d9ba47e0c9f487
2sick2speak <ma.kharchenko@gmail.com>
2014-11-13 17:04:39 +0400

    Read data && sample solution

---
+++ R/libraries.R
@@ -0,0 +1,7 @@
+# header for libraries
+
+require(ggplot2)
+require(tm)
+
+
+setwd("~/Dropbox/Public/DMTrack/Kaggle-ASAP-AES")
---
+++ R/solutions/0 sample solution.R
@@ -0,0 +1,3 @@
+source(paste0(getwd(), "/R/libraries.R"))
+source(paste0(getwd(), "/R/data processing/read_data.R"))
+       

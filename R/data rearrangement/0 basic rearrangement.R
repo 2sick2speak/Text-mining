81f99d06cffced7c119823c67a7135f01aee3655
2sick2speak <ma.kharchenko@gmail.com>
2014-11-17 14:39:44 +0400

    Function wrapping and small refactoring

--- R/data rearrangement/0 basic rearrangement.R
+++ R/data rearrangement/0 basic rearrangement.R
@@ -1,57 +1,32 @@
 source(paste0(getwd(), "/R/data rearrangement/_list stemming.R"))
 source(paste0(getwd(), "/R/data rearrangement/_stop words removal.R"))
 
-#additional stopwords
-# additional.stopwords <- c("dont")
+basicRearragements <- function(data, dictionary){
+  # erase @-like words 
+  data <- mapply(gsub, "@\\w+","",data)
+  
+  # remove punctuation  
+  data <- mapply(gsub, "\\W+"," ",data)
+  
+  # remove numbers
+  data <- mapply(gsub, "\\d+"," ",data)
 
-# make essay list of Corpus type
-train.corpus  <- essay.raw.data$train[which (essay.raw.data$train$essay_set == 8),] # use 8 type of essay
-test.corpus   <- essay.raw.data$test[which (essay.raw.data$test$essay_set == 8),]  # use 8 type of essay
-
-# erase @-like words 
-
-train.corpus[,"essay"] <- mapply(gsub, "@\\w+","",train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(gsub, "@\\w+","",test.corpus[,"essay"])
-
-# remove punctuation
-
-train.corpus[,"essay"] <- mapply(gsub, "\\W+"," ",train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(gsub, "\\W+"," ",test.corpus[,"essay"])
-
-# remove numbers
-
-train.corpus[,"essay"] <- mapply(gsub, "\\d+"," ",train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(gsub, "\\d+"," ",test.corpus[,"essay"])
-
-# to lower case
-
-train.corpus[,"essay"] <- mapply(tolower,train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(tolower,test.corpus[,"essay"])
-
-# remove stopwords
-# FUCKIN TIME CONSUMING OPERATION 20m+ for 8k texts. Better read from file 
-
-# FIXIT 
-# train.corpus[,"essay"] <- as.matrix(lapply(train.corpus[,"essay"], stopWordsRemove, stopwords("SMART")))[1,]
-# test.corpus[,"essay"] <- as.matrix(lapply(test.corpus[,"essay"], stopWordsRemove, stopwords("SMART")))[1,]
-
-
-# stem words
-
-train.corpus[,"essay"] <- mapply(stemmingToBigStrings, train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(stemmingToBigStrings, test.corpus[,"essay"])
-
-# erase whitespaces
-
-train.corpus[,"essay"] <- mapply(gsub, "\\s+"," ",train.corpus[,"essay"])
-test.corpus[,"essay"] <- mapply(gsub, "\\s+"," ",test.corpus[,"essay"])
-
-# return colnames
-
-colnames(train.corpus) <- names.storage
-colnames(test.corpus)  <- names.storage
+  # to lower case  
+  data <- mapply(tolower,data)
+  
+  # remove stopwords
+  # FUCKIN TIME CONSUMING OPERATION 20m+ for 8k texts. Better read from file 
+  # FIXIT 
+  # data <- as.matrix(lapply(data, stopWordsRemove, dictionary))[,1]
+    
+  # stem words
+  data <- mapply(stemmingToBigStrings, data)
+  
+  # erase whitespaces  
+  data <- mapply(gsub, "\\s+"," ",data)
+  
+}
 
 # TODO fix errors in words
 # TODO check stemming algorythms
-# TODO play with stopwords dictionary
 # TODO research sequence of text transformation for interesting cases
--- R/solutions/0 sample solution.R
+++ R/solutions/0 sample solution.R
@@ -2,3 +2,11 @@ source(paste0(getwd(), "/R/libraries.R"))
 source(paste0(getwd(), "/R/data processing/read_data.R"))
 source(paste0(getwd(), "/R/data rearrangement/0 basic rearrangement.R"))
 
+# make essay list of Corpus type
+train.corpus  <- essay.raw.data$train[which (essay.raw.data$train$essay_set == 8),] # use 8 type of essay
+test.corpus   <- essay.raw.data$test[which (essay.raw.data$test$essay_set == 8),]  # use 8 type of essay
+
+
+# TODO play with stopwords dictionary
+train.corpus[,"essay"] <- basicRearragements(train.corpus[,"essay"], stopwords("SMART")) 
+test.corpus[,"essay"]  <- basicRearragements(test.corpus[,"essay"], stopwords("SMART")) 
